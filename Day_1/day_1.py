fed6bd8bbce7be01f362ad5466463e506498b46a
Vincent Leclair <vinleclair@gmail.com>
2018-12-12 16:24:56 -0500

    Add input from file

---
+++ Day_1/day_1.py
@@ -0,0 +1,36 @@
+def main():
+
+    frequencies = parse_input_file()
+    current_frequency = 0
+    past_frequencies = []
+    calibrating = True
+
+    print("\nCalibrating...")
+    while calibrating:
+        for frequency in frequencies:
+            current_frequency += frequency
+
+            if current_frequency in past_frequencies:
+                calibrating = False
+                break
+
+            past_frequencies.append(current_frequency)
+
+    print("--- Puzzle Answers ---")
+    print("Part 1 :", sum(frequencies))
+    print("Part 2 :", current_frequency)
+
+
+def parse_input_file():
+
+    frequencies = []
+
+    with open(input("Name of input file: "), 'r') as fp:
+        for line in fp:
+            frequencies.append(int(line))
+
+    return frequencies
+
+
+if __name__ == "__main__":
+    main()
