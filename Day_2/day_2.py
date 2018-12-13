91f65594d16b3314c5487b5b925b1d4e5c4b3aac
Vincent Leclair <vinleclair@gmail.com>
2018-12-13 18:26:29 -0500

    Add day 2 part 2

--- Day_2/day_2.py
+++ Day_2/day_2.py
@@ -1,10 +1,20 @@
-def main():
+import itertools
 
-    ids_list = parse_input_file()
-    counting = True
+
+def main():
+    box_ids = parse_input_file()
+    print("--- Puzzle Answers ---")
+    print("Checksum :", find_checksum(box_ids))
+    print("Letters in common:", find_letters_in_common(box_ids))
+    return 0
+
+
+def find_checksum(box_ids):
+
     letter_count = {"doubles" : 0,
                     "triples" : 0}
-    for box_id in ids_list:
+
+    for box_id in box_ids:
         counted_double = False
         counted_triple = False
         for letter in box_id:
@@ -15,17 +25,32 @@ def main():
                 letter_count["triples"] += 1
                 counted_triple = True
 
-    print("Checksum :",letter_count["doubles"] * letter_count["triples"])
+    return letter_count["doubles"] * letter_count["triples"]
 
-    return 0
+
+def find_letters_in_common(box_ids):
+    for box_id_one, box_id_two in itertools.combinations(box_ids, 2):
+        if count_differences(box_id_one, box_id_two) == 1:
+            for first_letter, second_letter in zip(box_id_one, box_id_two):
+                if first_letter != second_letter:
+                    letters_in_common = box_id_one.replace(first_letter, "")
+    return letters_in_common
+
+
+def count_differences(first_string, second_string):
+    count_diffs = 0
+    for first_letter, second_letter in zip(first_string, second_string):
+        if first_letter != second_letter:
+            count_diffs += 1
+    return count_diffs
 
 
 def parse_input_file():
 
     box_ids = []
     try:
-        with open(input("Name of input file: "), 'r') as fp:
-            for line in fp:
+        with open(input("Name of input file: "), 'r') as file_pointer:
+            for line in file_pointer:
                 box_ids.append(line.rstrip())
     except FileNotFoundError:
         print("\nError: File not found.")
