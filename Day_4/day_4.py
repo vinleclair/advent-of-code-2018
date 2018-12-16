82c5fd16b090d4837ea2f1766cd0b18fac63876f
Vincent Leclair <vinleclair@gmail.com>
2018-12-16 12:22:14 -0500

    Refactor day 4

--- Day_4/day_4.py
+++ Day_4/day_4.py
@@ -1,111 +1,59 @@
-# SPAGHETTI CODE WARNING TURN BACK NOW YOU'VE BEEN WARNED
-
-import re
-import operator
+from collections import defaultdict
 
 
 def main():
-    records = parse_input_file()
-    guards = get_list_of_guards(records)
-    sleepy_guard = get_sleepy_guard(guards, records)
-    asleep_minute = find_most_asleep_minute(records, sleepy_guard)
-    for x in list(guards.keys()):
-        if guards[x] == 0:
-            del guards[x]
-    for guard in guards:
-        guards[guard] = {}
+    records = open_input_file()
+    most_asleep_minute, guard_sleep_minutes = get_time_dicts(records)
     print("---Puzzle Answers---")
-    print("Part 1:", asleep_minute * int(sleepy_guard.replace("#","")))
-    print("Part 2:", find_sleepiest_minute(guards, records))
+    print("Part 1:", arg_max(guard_sleep_minutes) * arg_max_minute(most_asleep_minute, arg_max(guard_sleep_minutes)))
+    print("Part 2:", arg_max(most_asleep_minute)[0] * arg_max(most_asleep_minute)[1])
 
 
-def find_sleepiest_minute(guards, records):
-    most_frequent_minute = 0
-    most_sleepy_minute = 0
-    for guard in guards:
-        guards[guard][find_most_asleep_minute(records, guard)] = find_asleep_frequency(records, guard)
-    for guard in guards:
-        if max(guards[guard].values()) > most_frequent_minute:
-            most_frequent_minute = max(guards[guard].values())
-            most_sleepy_minute = max(guards[guard].keys())
-            guard_id = guard
-    return int(guard_id.replace("#","")) * most_sleepy_minute
+def get_time_dicts(records):
+    guard_sleep_minutes = defaultdict(int)
+    most_asleep_minute = defaultdict(int)
+    guard = None
+    asleep = None
+    for line in records:
+        if line:
+            time = parse_time(line)
+            if 'begins shift' in line:
+                guard = int(line.split()[3][1:])
+                asleep = None
+            elif 'falls asleep' in line:
+                asleep = time
+            elif 'wakes up' in line:
+                for minute in range(asleep, time):
+                    most_asleep_minute[(guard, minute)] += 1
+                    guard_sleep_minutes[guard] += 1
+    return most_asleep_minute, guard_sleep_minutes
 
 
-def find_asleep_frequency(records, sleepy_guard):
-    sleepy_minutes = {}
-
-    for index, record in enumerate(records):
-        if len(re.findall(sleepy_guard, record)) > 0 and len(re.findall('falls asleep', records[index + 1])) > 0:
-            for minute in range(int(re.findall('\:\d+', records[index + 1])[0].replace(":","")), int(re.findall('\:\d+', records[index + 2])[0].replace(":",""))):
-                if minute not in sleepy_minutes:
-                    sleepy_minutes[minute] = 0
-                elif minute in sleepy_minutes:
-                    sleepy_minutes[minute] += 1
-
-        if len(re.findall(sleepy_guard, record)) > 0 and len(re.findall('falls asleep', records[index + 1])) > 0 and len(re.findall('falls asleep', records[index + 3])) > 0:
-            for minute in range(int(re.findall('\:\d+', records[index + 3])[0].replace(":","")), int(re.findall('\:\d+', records[index + 4])[0].replace(":",""))):
-                if minute not in sleepy_minutes:
-                    sleepy_minutes[minute] = 0
-                elif minute in sleepy_minutes:
-                    sleepy_minutes[minute] += 1
-    return max(sleepy_minutes.values())
+def arg_max(dictionary):
+    best = None
+    for key,value in dictionary.items():
+        if best is None or value > dictionary[best]:
+            best = key
+    return best
 
 
-def find_most_asleep_minute(records, sleepy_guard):
-    sleepy_minutes = {}
-
-    for index, record in enumerate(records):
-        if len(re.findall(sleepy_guard, record)) > 0 and len(re.findall('falls asleep', records[index + 1])) > 0:
-            for minute in range(int(re.findall('\:\d+', records[index + 1])[0].replace(":","")), int(re.findall('\:\d+', records[index + 2])[0].replace(":",""))):
-                if minute not in sleepy_minutes:
-                    sleepy_minutes[minute] = 0
-                elif minute in sleepy_minutes:
-                    sleepy_minutes[minute] += 1
-
-        if len(re.findall(sleepy_guard, record)) > 0 and len(re.findall('falls asleep', records[index + 1])) > 0 and len(re.findall('falls asleep', records[index + 3])) > 0:
-            for minute in range(int(re.findall('\:\d+', records[index + 3])[0].replace(":","")), int(re.findall('\:\d+', records[index + 4])[0].replace(":",""))):
-                if minute not in sleepy_minutes:
-                    sleepy_minutes[minute] = 0
-                elif minute in sleepy_minutes:
-                    sleepy_minutes[minute] += 1
-    return max(sleepy_minutes.items(), key=operator.itemgetter(1))[0]
+def arg_max_minute(dictionary, guard):
+    best = None
+    for key,value in dictionary.items():
+        if (best is None or value > dictionary[best]) and key[0] == guard:
+            best = key
+    return best[1]
 
 
-def get_sleepy_guard(guards, records):
-    for index, record in enumerate(records):
-        try:
-            if len(re.findall('\#\d+', record)) > 0 and len(re.findall('falls asleep', records[index + 1])) > 0:
-                guards[re.findall('\#\d+', record)[0]] += int(re.findall('\:\d+', records[index + 2])[0].replace(":","")) - int(re.findall('\:\d+', records[index + 1])[0].replace(":",""))
-            if len(re.findall('\#\d+', record)) > 0 and len(re.findall('falls asleep', records[index + 1])) > 0 and len(re.findall('falls asleep', records[index + 3])) > 0:
-                guards[re.findall('\#\d+', record)[0]] += int(re.findall('\:\d+', records[index + 4])[0].replace(":","")) - int(re.findall('\:\d+', records[index + 3])[0].replace(":",""))
-        except IndexError:
-            print("GIMME A BREAK GIMMME A BREAK BREAK BREAK BREAK")
-            break
-    return max(guards.items(), key=operator.itemgetter(1))[0]
+def parse_time(line):
+    words = line.split()
+    date, time = words[0][1:], words[1][:-1]
+    return int(time.split(':')[1])
 
 
-def get_list_of_guards(records):
-    guards = {}
+def open_input_file():
 
-    for record in records:
-        if len(re.findall('\#\d+', record)) > 0:
-            guard = re.findall('\#\d+', record)[0]
-            if guard not in records:
-                guards[guard] = 0
-    return guards
-
-
-def parse_input_file():
-
-    records = []
-    try:
-        with open(input("Name of input file: "), 'r') as file_pointer:
-            for line in file_pointer:
-                records.append(line)
-    except FileNotFoundError:
-        print("\nError: File not found.")
-        exit(1)
+    records = open('input.txt').read().split('\n')
     return sorted(records)
 
 
