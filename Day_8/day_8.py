bc42e3dd452efb5896c70ab56d5bf5c2e4713080
Vincent Leclair <vinleclair@gmail.com>
2019-01-02 17:14:10 -0500

    Add partial day 8 part 1

--- Day_8/day_8.py
+++ Day_8/day_8.py
@@ -4,7 +4,32 @@ import re
 
 def main():
     license = parse_input_file()
+    print(part_1(license))
     return 0
+
+
+def part_1(license):
+    cursor = 0
+    child_nodes_qty = 0
+    metadata_quantity = 0
+    total = 0
+    for index, number in enumerate(license):
+        if cursor == 0:
+            child_nodes_qty = license[index]
+            metadata_qty = license[index + 1]
+            cursor = child_nodes_qty + 2
+            for range_index in range(metadata_qty):
+                print(license[range_index])
+                total += license[range_index]
+                cursor += 1
+            index = cursor
+        else:
+            cursor = 0
+        if index > len(license):
+            break
+
+    return total
+        
 
 
 def parse_input_file():
