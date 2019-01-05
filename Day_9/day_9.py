22d1fffaf556b1a47cb23b65dc0e6e83c392d906
Vincent Leclair <vinleclair@gmail.com>
2019-01-05 16:42:08 -0500

    Complete day 9 part 1 & 2

--- Day_9/day_9.py
+++ Day_9/day_9.py
@@ -1,11 +1,27 @@
+from collections import deque
 import fileinput
 import re
 
 
 def main():
     num_players, last_marble = parse_input()
-    print("Players:", num_players)
-    print("Marbles:", last_marble)
+    print("Part 1:", marble_game(num_players, last_marble))
+    print("Part 2:", marble_game(num_players, last_marble * 100))
+
+
+def marble_game(num_players, last_marble):
+    circle = deque([0])
+    scores = [0] * num_players
+    for marble in range(1, last_marble + 1):
+        if marble % 23 == 0:
+            circle.rotate(7)
+            scores[marble % num_players] += marble + circle.pop()
+            circle.rotate(-1)
+        else:
+            circle.rotate(-1)
+            circle.append(marble)
+
+    return max(scores)
 
 
 def parse_input():
