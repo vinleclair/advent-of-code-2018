e04fffc521211d1cf828a17391423706aabcdb08
Vincent Leclair <vinleclair@gmail.com>
2019-01-21 16:05:28 -0500

    Complete day 11 part 1 & 2

--- Day_11/day_11.py
+++ Day_11/day_11.py
@@ -1,9 +1,25 @@
 from collections import defaultdict
 import fileinput
 
+
 def main():
     grid = initialize_summed_area_table(int(next(fileinput.input())))
-    print(grid)
+    print('Part 1: %d,%d' % find_max_power(grid, 3)[1:])
+    print('Part 2: %d,%d,%d' % max(find_max_power(grid, size) + (size,) for size in range(1, 301))[1:])
+
+
+def find_max_power(grid, size):
+    power = []
+    for y in range(1, 301 - size + 1):
+        for x in range(1, 301 - size + 1):
+            region = calculate_region(grid, size, x, y)
+            power.append((region, x, y))
+    return max(power)
+
+
+def calculate_region(grid, size, x, y):
+    x0, y0, x1, y1 = x - 1, y - 1, x + size - 1, y + size - 1
+    return grid[(x0, y0)] + grid[(x1, y1)] - grid[(x1, y0)] - grid[(x0, y1)]
 
 
 def initialize_summed_area_table(serial_number):
